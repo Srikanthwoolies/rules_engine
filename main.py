dbbead58d1b18778167bfd636069a9f679fad9e0
Srikanthwoolies <sramesh6@woolworths.com.au>
2025-04-03 17:11:01 +1100

    cleanup

---
+++ main.py
@@ -0,0 +1,5 @@
+"""
+Cloud Function entry point for the GCS Rules Engine.
+This module contains the main Cloud Function that is triggered by GCS events.
+"""
+print("test")
--- src/rules_engine/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Rules Engine package for processing GCS files and applying BigQuery rules."""
-
-__version__ = "0.1.0"
